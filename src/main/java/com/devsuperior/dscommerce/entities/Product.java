1475f44958a2bd3e3422e249711acb0095018dd1
Breno Silva <brenolevy292@gmail.com>
2025-09-29 23:07:03 -0400

    OrderItem and OrderItemPK

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -28,6 +28,9 @@ public class Product {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product() {
     }
 
@@ -82,4 +85,6 @@ public class Product {
     public Set<Category> getCategories() {
         return categories;
     }
+
+
 }
